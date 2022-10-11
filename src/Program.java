1543dca437e9fc3fd3baf19226a25b1dfe85a0ed
AysegulKurt35 <aysglkrt1988@gmail.com>
2022-10-11 21:22:01 +0300

    aysegul degistirildi

--- src/Program.java
+++ src/Program.java
@@ -2,5 +2,12 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Yeni proje'den herkese merhaba!");
         System.out.println("-------------------------------");
+        System.out.println("11 ekim");
+
+
     }
+
+
+
+
 }
