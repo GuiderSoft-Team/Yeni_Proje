9114671e1c4e4f3315cd47fe4e217d01c66d81cb
Mustafa ASLAN <mmustafaslann1453@gmail.com>
2022-10-11 21:21:44 +0300

    mustafa değiştirdi

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,7 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Yeni proje'den herkese merhaba!");
-        System.out.println("-------------------------------");
+        System.out.println("yeni bir proje");
+        System.out.println("merhabalar");
     }
 }
